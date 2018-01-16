0e961bd6a70828c09e25f04ad73ae3ecd01a3db8
Ion <i.cojucovschi@software-dep.net>
2018-01-16 13:02:19 +0200

    transform image from bitmap to  byte array

--- CoreIon/Core.Android/Animations/CirclesFadeInOutAnimation.cs
+++ CoreIon/Core.Android/Animations/CirclesFadeInOutAnimation.cs
@@ -1,18 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
-namespace Core.Android.Animation
+﻿using Android.Views;
+using Android.Views.Animations;
+namespace Core.Android.Animations
 {
-    class CirclesFadeInOutAnimation
+    public static class CirclesFadeInOutAnimation
     {
+        public static void AnimateViews(int duration = 300, params View[] views)
+        {
+            var fadeInAnims = new AlphaAnimation[views.Length];
+            var fadeOutAnims = new AlphaAnimation[views.Length];
+            for (var i = 0; i < views.Length; i++)
+            {
+                fadeInAnims[i] = new AlphaAnimation(0.0f, 1.0f);
+                fadeOutAnims[i] = new AlphaAnimation(1.0f, 0.0f);
+
+                fadeInAnims[i].Duration = duration;
+                fadeOutAnims[i].Duration = duration;
+
+                fadeInAnims[i].FillAfter = true;
+                fadeOutAnims[i].FillAfter = true;
+                views[i].Tag = i;
+            }
+
+            for (var i = 0; i < views.Length; i++)
+            {
+                if (i == 0)
+                {
+                    fadeInAnims[0].AnimationEnd += (sender, e) => { views[0].StartAnimation(fadeOutAnims[0]); };
+                }
+                else
+                {
+                    var pos = (int)views[i].Tag;
+                    fadeInAnims[pos].AnimationEnd += (sender, e) =>
+                    {
+                        views[pos - 1].StartAnimation(fadeInAnims[pos - 1]);
+                    };
+                }
+
+                if (i == views.Length - 1)
+                {
+                    fadeOutAnims[views.Length - 1].AnimationEnd += (sender, e) =>
+                    {
+                        views[views.Length - 1].StartAnimation(fadeInAnims[views.Length - 1]);
+                    };
+                }
+                else
+                {
+                    var pos = (int)views[i].Tag;
+                    fadeOutAnims[pos].AnimationEnd += (sender, e) =>
+                    {
+                        views[pos + 1].StartAnimation(fadeOutAnims[pos + 1]);
+                    };
+                }
+
+                views[0].StartAnimation(fadeOutAnims[0]);
+            }
+        }
     }
 }
---
+++ CoreIon/Core.Android/ImagesUtility/TransformImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Core.Android.ImagesUtility
+{
+   public class TransformImage
+    {
+        byte[] byteIMage;
+        Bitmap ImageBitmap;
+
+
+
+        public TransformImage(Bitmap imgbtm)
+        {
+            ImageBitmap = imgbtm;
+        }
+
+        public byte[] BitmapToByte(Bitmap btmp)
+        {
+            using (var bite = new System.IO.MemoryStream())
+            {
+                btmp.Compress(Bitmap.CompressFormat.Jpeg, 0, bite);
+                byteIMage = bite.ToArray();
+            }
+            return byteIMage;
+        }
+
+
+    }
+}
